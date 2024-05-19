a9782290ff47f6654317425e6692fea128fb80c0
Timothy Small <smalls.online@icloud.com>
2024-05-19 16:52:59 -0400

    Modify how options are added for CSharpAddProjectCommand

--- src/Configurator/Commands/CsharpCommand/CsharpAddProjectCommand/CsharpAddProjectCommand.cs
+++ src/Configurator/Commands/CsharpCommand/CsharpAddProjectCommand/CsharpAddProjectCommand.cs
@@ -14,7 +14,27 @@ public class CSharpAddProjectCommand : CliCommand
     {
         Description = "Add a new C# project to the workspace.";
 
-        Options.Add(
+        Options
+            .AddSolutionFilePathOption()
+            .AddProjectPathOption()
+            .AddProjectFriendlyNameOption()
+            .AddIsRunnableOption()
+            .AddIsWatchableOption();
+
+        Action = new CSharpAddProjectCommandAction();
+    }
+}
+
+file static class CSharpAddProjectCommandExtensions
+{
+    /// <summary>
+    /// Add '--solution-file-path' option to the command.
+    /// </summary>
+    /// <param name="options">The options to add to.</param>
+    /// <returns>The <see cref="IList{T}"/> for chaining.</returns>
+    public static IList<CliOption> AddSolutionFilePathOption(this IList<CliOption> options)
+    {
+        options.Add(
             new CliOption<string>("--solution-file-path")
             {
                 Description = "The solution file to add the project to.",
@@ -22,7 +42,17 @@ public class CSharpAddProjectCommand : CliCommand
             }
         );
 
-        Options.Add(
+        return options;
+    }
+
+    /// <summary>
+    /// Add '--project-path' option to the command.
+    /// </summary>
+    /// <param name="options">The options to add to.</param>
+    /// <returns>The <see cref="IList{T}"/> for chaining.</returns>
+    public static IList<CliOption> AddProjectPathOption(this IList<CliOption> options)
+    {
+        options.Add(
             new CliOption<string>("--project-path")
             {
                 Description = "The path to the project.",
@@ -30,7 +60,17 @@ public class CSharpAddProjectCommand : CliCommand
             }
         );
 
-        Options.Add(
+        return options;
+    }
+
+    /// <summary>
+    /// Add '--project-friendly-name' option to the command.
+    /// </summary>
+    /// <param name="options">The options to add to.</param>
+    /// <returns>The <see cref="IList{T}"/> for chaining.</returns>
+    public static IList<CliOption> AddProjectFriendlyNameOption(this IList<CliOption> options)
+    {
+        options.Add(
             new CliOption<string>("--project-friendly-name")
             {
                 Description = "The friendly name of the project.",
@@ -38,7 +78,17 @@ public class CSharpAddProjectCommand : CliCommand
             }
         );
 
-        Options.Add(
+        return options;
+    }
+
+    /// <summary>
+    /// Add '--is-runnable' option to the command.
+    /// </summary>
+    /// <param name="options">The options to add to.</param>
+    /// <returns>The <see cref="IList{T}"/> for chaining.</returns>
+    public static IList<CliOption> AddIsRunnableOption(this IList<CliOption> options)
+    {
+        options.Add(
             new CliOption<bool>("--is-runnable")
             {
                 Description = "Whether the project is runnable.",
@@ -47,7 +97,17 @@ public class CSharpAddProjectCommand : CliCommand
             }
         );
 
-        Options.Add(
+        return options;
+    }
+
+    /// <summary>
+    /// Add '--is-watchable' option to the command.
+    /// </summary>
+    /// <param name="options">The options to add to.</param>
+    /// <returns>The <see cref="IList{T}"/> for chaining.</returns>
+    public static IList<CliOption> AddIsWatchableOption(this IList<CliOption> options)
+    {
+        options.Add(
             new CliOption<bool>("--is-watchable")
             {
                 Description = "Whether the project is watchable.",
@@ -56,6 +116,6 @@ public class CSharpAddProjectCommand : CliCommand
             }
         );
 
-        Action = new CSharpAddProjectCommandAction();
+        return options;
     }
 }
